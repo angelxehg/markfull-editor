001faae170f520c641f5edfdbd1c59aa2dee4130
Angel Hurtado <sudo@angelxehg.com>
2021-05-18 10:20:13 -0500

    Instalar Bootstrap y React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const App = (): JSX.Element => {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <header>
         <h1>Hola mundo!</h1>
         <p>Este es un editor de Markdown con vista previa</p>
       </header>
+      <Button onClick={() => setCount(count + 1)}>
+        {count > 0 ? `${count} clicks!` : 'Dame click'}
+      </Button>
     </div>
   );
 }
