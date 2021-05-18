ff773d1667bac2866dd71f29608a22d9442be76d
Angel Hurtado <sudo@angelxehg.com>
2021-05-18 12:37:57 -0500

    Ajustes y Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import TextareaAutosize from 'react-textarea-autosize';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 
 import { basicExample } from './examples';
@@ -16,19 +17,24 @@ const App = (): JSX.Element => {
   const [inputContent, setInputContent] = useState(basicExample);
   return (
     <div>
+      <Navbar bg="dark" variant="dark" expand="sm" sticky="top">
+        <Container fluid>
+          <Navbar.Brand>Markfull</Navbar.Brand>
+        </Container>
+      </Navbar>
       <Container fluid className="pt-3 pb-3">
         <Row>
           <Col md="6">
-            <div className="mb-3">
-              <label htmlFor="inputContent" className="form-label">Contenido:</label>
-              <TextareaAutosize
-                id="inputContent"
-                className="form-control"
-                rows={20}
-                value={inputContent}
-                onChange={(e) => setInputContent(e.target.value)}
-              />
-            </div>
+            <TextareaAutosize
+              id="inputContent"
+              className="form-control"
+              style={{
+                overflow: 'hidden'
+              }}
+              rows={20}
+              value={inputContent}
+              onChange={(e) => setInputContent(e.target.value)}
+            />
           </Col>
           <Col md="6">
             <ReactMarkdown components={{
