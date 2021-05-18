78886ce20013fcdabceab9d73e8a0a27955dd102
Angel Hurtado <sudo@angelxehg.com>
2021-05-18 10:24:47 -0500

    Optimizar import react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 const App = (): JSX.Element => {
   const [count, setCount] = useState(0);
