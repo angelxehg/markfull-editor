0c7204fe6989c8fab3dd4d64439f6723df73e4b5
Angel Hurtado <sudo@angelxehg.com>
2021-05-18 12:39:30 -0500

    Ejemplo más corto

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -2,23 +2,13 @@ export const basicExample = `# Maxime quia
 
 Maxime quia distinctio neque aut ab nobis. Libero non eius qui dolore voluptatibus molestiae. Beatae doloremque doloribus qui quis ex qui recusandae.
 
+- [Enlace](https://angelxehg.com)
+
 ## Vel dolore
 
 Vel dolore quasi numquam quia nihil. Esse quidem et recusandae id vel reiciendis. Autem at iusto exercitationem sed non ut. Corrupti et aut qui est ipsum quis architecto. Totam veritatis provident est deserunt aliquam unde velit. Suscipit id ut dolores porro quia.
 
-## Quis eum
-
-Quis eum voluptatem dolores dolore qui. Aliquam nostrum nulla labore blanditiis. Qui qui necessitatibus voluptate inventore. Totam cupiditate porro eum qui corporis mollitia facere blanditiis. Repudiandae quis vel modi quibusdam et autem tenetur qui. Iusto nisi inventore repellendus consequatur.
-
 ![Pomeranian working on an iPad](https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29tcHV0ZXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)
 
 > Photo by [Cookie the Pom](https://unsplash.com/@cookiethepom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on [Unsplash](https://unsplash.com/s/photos/computer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
-
-### Corrupti nisi
-
-Corrupti nisi soluta consequatur nesciunt aut. Id id tempora error quos. Et iste ullam et porro atque illo distinctio. Ut nihil rerum dolor dolorem eum aut hic.
-
-### Omnis velit
-
-Omnis velit velit voluptatem. Atque omnis soluta ex qui totam dolor quasi. Quo quia architecto odit maxime cupiditate reiciendis.
 `
