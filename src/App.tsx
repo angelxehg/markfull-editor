97ff4bfcbb5adf0a091dd079c6f65abcd7cbc457
Angel Hurtado <sudo@angelxehg.com>
2021-05-18 10:14:40 -0500

    Quitar plantilla inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Hola mundo!</h1>
+        <p>Este es un editor de Markdown con vista previa</p>
       </header>
     </div>
   );
